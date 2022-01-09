80920fbdc8d727d8d8a2abddc9efe9e49d3e8092
darcy561 <86856265+darcy561@users.noreply.github.com>
2022-01-09 23:05:17 +0000

    Comit

--- src/Components/Home/Components/LoggedOut.jsx
+++ src/Components/Home/Components/LoggedOut.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { Box, Grid, Paper, Typography } from "@mui/material";
+import { Box, Divider, Grid, Icon, Paper, Typography } from "@mui/material";
+import { useMediaQuery, useTheme } from "@mui/material";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
 export function LoggedOutHome() {
+  const theme = useTheme();
+  const mobile = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <Grid
       container
@@ -27,14 +33,11 @@ export function LoggedOutHome() {
           square={true}
         >
           <Grid item xs={12}>
-            <Typography variant="h2" align="center" color="primary">
-              Eve Industry Planner
-            </Typography>
             <Typography
               variant="body2"
               align="center"
               color="primary"
-              sx={{ fontStyle: "italic" }}
+              sx={{ fontStyle: "italic", marginBottom: "10px" }}
             >
               Making the spreadsheet, so you dont have to!
             </Typography>
@@ -47,7 +50,7 @@ export function LoggedOutHome() {
               Alpha Release
             </Typography>
           </Grid>
-          <Grid container item xs={12} sx={{ marginTop: "50px" }}>
+          <Grid container item xs={12} sx={{ marginTop: "30px" }}>
             <Typography paragraph variant="body1" align="left">
               Welcome to Eve Industry Planner a new way to plan and manage your
               industry jobs quickly so that you can spend more time in game
@@ -71,42 +74,130 @@ export function LoggedOutHome() {
               Eve Industry Planner takes the whole process and breaks it down
               into 5 simple steps:
             </Typography>
-            <Grid item xs={12} sm={6}>
-              <Typography paragraph variant="body1">
-                <i>
-                  <b>Planning</b>
-                </i>{" "}
-                - Here we can work out how many runs, job slots, the different
-                structure types, all of this will is used to calculate the
-                resources to create your item.
-              </Typography>
+          </Grid>
+          <Grid container item xs={12}>
+            <Grid container item xs={12}>
+              <Grid item xs={3} sm={0} sx={{display:{xs:"block", sm:"none"}}}/>
+              <Grid item xs={6} sm={12} >
+                <Paper
+                  elevation={1}
+                  square={true}
+                  sx={{
+                    padding: "20px",
+                    marginTop: "10px",
+                    marginBottom: "30px",
+                  }}
+                >
+                  <Grid container item xs={12} columns={9}>
+                    <Grid item xs={12} sm={1} align="center">
+                      <Typography variant="h6" color="primary">
+                        Plan
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={1} align="center">
+                      {mobile ? (
+                        <Icon color="secondary">
+                          <ArrowDownwardIcon />
+                        </Icon>
+                      ) : (
+                        <Icon color="secondary">
+                          <ArrowForwardIcon />
+                        </Icon>
+                      )}
+                    </Grid>
+                    <Grid item xs={12} sm={1} align="center">
+                      <Typography variant="h6" color="primary">
+                        Purchase
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={1} align="center">
+                    {mobile ? (
+                        <Icon color="secondary">
+                          <ArrowDownwardIcon />
+                        </Icon>
+                      ) : (
+                        <Icon color="secondary">
+                          <ArrowForwardIcon />
+                        </Icon>
+                      )}
+                    </Grid>
+                    <Grid item xs={12} sm={1} align="center">
+                      <Typography variant="h6" color="primary">
+                        Build
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={1} align="center">
+                    {mobile ? (
+                        <Icon color="secondary">
+                          <ArrowDownwardIcon />
+                        </Icon>
+                      ) : (
+                        <Icon color="secondary">
+                          <ArrowForwardIcon />
+                        </Icon>
+                      )}
+                    </Grid>
+                    <Grid item xs={12} sm={1} align="center">
+                      <Typography variant="h6" color="primary">
+                        Review
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={1} align="center">
+                    {mobile ? (
+                        <Icon color="secondary">
+                          <ArrowDownwardIcon />
+                        </Icon>
+                      ) : (
+                        <Icon color="secondary">
+                          <ArrowForwardIcon />
+                        </Icon>
+                      )}
+                    </Grid>
+                    <Grid item xs={12} sm={1} align="center">
+                      <Typography variant="h6" color="primary">
+                        Sell
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Paper>
+              </Grid>
+              <Grid item xs={3} sm={0} sx={{display:{xs:"block", sm:"none"}}}></Grid>
             </Grid>
-                      <Grid item xs={12} sm={6}>
-                          Image
-                {/* <img src="\images\loggedOutHome\planningStepImage.png" alt="" /> */}
+            <Grid container item xs={12}>
+              <Grid item xs={12} sm={3}>
+                <Typography paragraph variant="body1">
+                  <i>
+                    <b>Planning</b>
+                  </i>{" "}
+                  - Here we can work out how many runs, job slots, the different
+                  structure types, all of this will is used to calculate the
+                  resources to create your item.
+                </Typography>
+              </Grid>
+              <Grid item xs={9} align="center">
+                <img src="\images\loggedOutHome\planningStepImage.png" alt="" />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={6}>
-                          <Box sx={{ maxWidth: "10%" }}>
-                              image
-                {/* <img
+            <Grid container item xs={12}>
+              <Grid item xs={12} sm={9} align="center">
+                <img
                   src="\images\loggedOutHome\purchasingStepImage.png"
                   alt=""
-                /> */}
-              </Box>
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <Typography paragraph variant="body1">
+                  <i>
+                    <b>Purchasing</b>
+                  </i>{" "}
+                  - Once you know what you are building then it is time to go
+                  out and purchase the items that you need. Enter the item costs
+                  for each material until you have all the materials you need
+                  and your total raw materials cost.
+                </Typography>
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <Typography paragraph variant="body1">
-                <i>
-                  <b>Purchasing</b>
-                </i>{" "}
-                - Once you know what you are building then it is time to go out
-                and purchase the items that you need. Enter the item costs for
-                each material until you have all the materials you need and your
-                total raw materials cost.
-              </Typography>
-            </Grid>
-
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12}>
               <Typography paragraph variant="body1">
                 <i>
                   <b>Building</b>
@@ -116,13 +207,9 @@ export function LoggedOutHome() {
                 display the completion time/date.
               </Typography>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <Box>image</Box>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Box>image</Box>
-            </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12}></Grid>
+
+            <Grid item xs={12}>
               <Typography paragraph variant="body1">
                 <i>
                   <b>Complete</b>
@@ -132,7 +219,8 @@ export function LoggedOutHome() {
                 remove before moving it to the final stage, the fun part.
               </Typography>
             </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12}></Grid>
+            <Grid item xs={12}>
               <Typography paragraph variant="body1">
                 <i>
                   <b>Sale</b>
@@ -144,9 +232,7 @@ export function LoggedOutHome() {
                 of your job.
               </Typography>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <Box>image    </Box>
-            </Grid>
+            <Grid item xs={12}></Grid>
           </Grid>
           <Grid item xs={12} sx={{ marginTop: "10px" }}>
             <Typography paragraph variant="body1" align="left">
@@ -155,6 +241,7 @@ export function LoggedOutHome() {
           </Grid>
         </Paper>
       </Grid>
+
     </Grid>
   );
 }

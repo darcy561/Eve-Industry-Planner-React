c9a897d0606ab66c3ecee7e3d56def58a8770b0c
darcy561 <86856265+darcy561@users.noreply.github.com>
2022-04-22 13:54:14 +0100

    Commit

---
+++ src/Components/Feedback/feedback.jsx
@@ -0,0 +1,77 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Fab,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
+import { doc, setDoc } from "firebase/firestore";
+import { firestore } from "../../firebase";
+
+export function FeedbackIcon() {
+  const [open, setOpen] = useState(false);
+  const [inputText, updateInputText] = useState("");
+
+  const handleSubmit = async () => {
+    await setDoc(doc(firestore, "Feedback", Date.now().toString()), {
+      response: inputText,
+    });
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Fab
+        color="primary"
+        size="small"
+        variant="extended"
+        sx={{ position: "fixed", bottom: "10px ", right: "5px" }}
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
+        Feedback
+      </Fab>
+
+      <Dialog
+        open={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+      >
+        <DialogTitle color="primary" align="center">
+          Feedback
+        </DialogTitle>
+        <DialogContent>
+          As development continues I would love to hear back from you with ideas
+          or thoughts regarding this application.
+        </DialogContent>
+        <DialogActions>
+          <TextField
+            multiline
+            minRows={5}
+            fullWidth
+            onChange={(e) => {
+              updateInputText(e.target.value);
+            }}
+          />
+        </DialogActions>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              setOpen(false);
+            }}
+          >
+            Close
+          </Button>
+          <Button variant="contained" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}

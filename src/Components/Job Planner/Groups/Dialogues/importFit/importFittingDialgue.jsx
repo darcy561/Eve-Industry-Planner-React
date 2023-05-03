9a77e355241eff117924215ff9f487677a1ce0b8
James Darcy <darcy561@gmail.com>
2023-05-03 20:06:55 +0100

    Commit

--- src/Components/Job Planner/Groups/Dialogues/importFit/importFittingDialgue.jsx
+++ src/Components/Job Planner/Groups/Dialogues/importFit/importFittingDialgue.jsx
@@ -4,12 +4,27 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
+  FormControl,
   Grid,
+  TextField,
   Typography,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useImportFitFromClipboard } from "../../../../../Hooks/GroupHooks/useImportFitFromClipboard";
 import { ImportFittingItemRow } from "./importFittingItemRow";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  TextField: {
+    "& .MuiFormHelperText-root": {
+      color: theme.palette.secondary.main,
+    },
+    "& input::-webkit-clear-button, & input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button":
+      {
+        display: "none",
+      },
+  },
+}));
 
 export function ImportItemFitDialogue({
   importFitDialogueTrigger,
@@ -18,9 +33,15 @@ export function ImportItemFitDialogue({
   const [clipboardPermissionDisabled, updateClipboardPermissionDisabled] =
     useState(false);
   const [importedItemList, updateImportedItemList] = useState([]);
-  const { importFromClipboard } = useImportFitFromClipboard();
+  const [fitQuantityMultiplier, updateFitQuantityMultiplier] = useState(1);
+  const { finalBuildRequests, importFromClipboard } =
+    useImportFitFromClipboard();
+  const classes = useStyles();
 
   const handleClose = () => {
+    updateClipboardPermissionDisabled(false);
+    updateImportedItemList([]);
+    updateFitQuantityMultiplier(1);
     updateImportFitDialogueTrigger((prev) => !prev);
   };
 
@@ -46,27 +67,73 @@ export function ImportItemFitDialogue({
     }
     checkClipboardPermission();
   }, [importFitDialogueTrigger]);
-
   return (
     <Dialog
       open={importFitDialogueTrigger}
       onClose={handleClose}
       sx={{ padding: "20px" }}
     >
-      <DialogTitle>Import Fit</DialogTitle>
+      <DialogTitle color="primary" align="center">
+        Import Fit
+      </DialogTitle>
       <DialogContent>
         {clipboardPermissionDisabled ? (
           <Typography> No Access To Clipboard</Typography>
         ) : (
           <Grid container>
-            {importedItemList.map((item) => {
-              return <ImportFittingItemRow item={item} />;
+            {importedItemList.map((item, index) => {
+              return (
+                <ImportFittingItemRow
+                  key={item.itemID}
+                  updateImportedItemList={updateImportedItemList}
+                  item={item}
+                  index={index}
+                />
+              );
             })}
           </Grid>
         )}
       </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Close</Button>
+      <DialogActions sx={{ padding: "20px" }}>
+        <Grid container>
+          <Grid item xs={3}>
+            <TextField
+              fullWidth
+              className={classes.TextField}
+              size="size"
+              variant="standard"
+              helperText="Fit Quantity"
+              type="number"
+              value={fitQuantityMultiplier}
+              InputProps={{ inputProps: { step: "1", min: 1 } }}
+              onChange={(e) => {
+                const newItemArray = importedItemList.map((entry) => {
+                  entry.itemCalculatedQty =
+                    entry.itemBaseQty * Math.round(e.target.value);
+                  return entry;
+                });
+                updateImportedItemList(newItemArray);
+                updateFitQuantityMultiplier(Math.round(e.target.value));
+              }}
+            />
+          </Grid>
+          <Grid item xs={3} />
+          <Grid item xs={4} align="center">
+            <Button
+              size="small"
+              variant="contained"
+              onClick={async () => {
+                await finalBuildRequests(importedItemList);
+                handleClose();
+              }}
+            >
+              Import Items
+            </Button>
+          </Grid>
+          <Grid item xs={2} align="center">
+            <Button onClick={handleClose}>Close</Button>
+          </Grid>
+        </Grid>
       </DialogActions>
     </Dialog>
   );
--- src/Hooks/GroupHooks/useImportFitFromClipboard.jsx
+++ src/Hooks/GroupHooks/useImportFitFromClipboard.jsx
@@ -1,9 +1,26 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import itemTypes from "../../RawData/searchIndex.json";
-import { ActiveJobContext } from "../../Context/JobContext";
+import { ActiveJobContext, JobArrayContext } from "../../Context/JobContext";
+import { useJobBuild } from "../useJobBuild";
+import { useBlueprintCalc } from "../useBlueprintCalc";
+import { useJobManagement } from "../useJobManagement";
+import { IsLoggedInContext, UsersContext } from "../../Context/AuthContext";
+import { useFirebase } from "../useFirebase";
+import { EvePricesContext } from "../../Context/EveDataContext";
 
 export function useImportFitFromClipboard() {
-  const { activeGroup } = useContext(ActiveJobContext);
+  const { activeGroup, updateActiveGroup } = useContext(ActiveJobContext);
+  const { jobArray, updateJobArray, groupArray, updateGroupArray } =
+    useContext(JobArrayContext);
+  const { users } = useContext(UsersContext);
+  const { updateEvePrices } = useContext(EvePricesContext);
+  const { isLoggedIn } = useContext(IsLoggedInContext);
+  const { buildJob, recalculateItemQty } = useJobBuild();
+  const { CalculateResources, CalculateTime } = useBlueprintCalc();
+  const { generatePriceRequestFromJob } = useJobManagement();
+  const { addNewJob, getItemPrices } = useFirebase();
+
+  const parentUser = useMemo(() => users.find((i) => i.ParentUser), [users]);
 
   const importFromClipboard = async () => {
     const itemNameRegex = /^\[(?<itemName>.+),\s*(?<fittingName>.+)\]/g;
@@ -16,10 +33,6 @@ export function useImportFitFromClipboard() {
     const itemsWithQuantities = [
       ...importedText.matchAll(itemWithQuantitiesRegex),
     ];
-      
-      console.log(itemNameMatch)
-      console.log(itemMatches)
-      console.log(itemsWithQuantities)
 
     if (itemNameMatch.length === 0) {
       return [];
@@ -30,23 +43,44 @@ export function useImportFitFromClipboard() {
       .map((match) => match[0].trim());
 
     const objectArray = [
-      { itemName: itemNameMatch[0].groups.itemName, itemQty: 1 },
+      {
+        itemName: itemNameMatch[0].groups.itemName,
+        itemBaseQty: 1,
+        itemCalculatedQty: 1,
+        included: true,
+      },
     ];
 
     filteredItemMatches.forEach((itemName) => {
       const foundItem = objectArray.find((item) => item.itemName === itemName);
       if (foundItem) {
-        foundItem.itemQty += 1;
+        foundItem.itemBaseQty += 1;
+        foundItem.itemCalculatedQty += 1;
       } else {
-        objectArray.push({ itemName, itemQty: 1 });
+        objectArray.push({
+          itemName,
+          itemBaseQty: 1,
+          itemCalculatedQty: 1,
+          included: true,
+        });
       }
     });
 
     itemsWithQuantities.forEach((match) => {
-      objectArray.push({
-        itemName: match.groups.item,
-        quantity: match.groups.quantity,
-      });
+      const foundItem = objectArray.find(
+        (item) => item.itemName === match.groups.item
+      );
+      if (foundItem) {
+        foundItem.itemBaseQty += Number(match.groups.quantity);
+        foundItem.itemCalculatedQty += Number(match.groups.quantity);
+      } else {
+        objectArray.push({
+          itemName: match.groups.item,
+          itemBaseQty: Number(match.groups.quantity),
+          itemCalculatedQty: Number(match.groups.quantity),
+          included: true,
+        });
+      }
     });
 
     for (let i = objectArray.length - 1; i >= 0; i--) {
@@ -59,12 +93,148 @@ export function useImportFitFromClipboard() {
         objectArray.splice(i, 1);
       }
     }
-      console.log(objectArray);
-    
-    return objectArray
+
+    return objectArray;
   };
-    
+
+  const finalBuildRequests = async (itemArray) => {
+    if (!itemArray) return;
+    let newJobArray = [...jobArray];
+    let newPriceIDs = new Set();
+    let jobsToSave = new Set();
+
+    const buildRequests = itemArray
+      .map((itemEntry) => {
+        if (itemEntry.included) {
+          return {
+            itemID: itemEntry.itemID,
+            itemQty: itemEntry.itemCalculatedQty,
+            groupID: activeGroup.groupID,
+          };
+        }
+      })
+      .filter((entry) => entry);
+
+    const groupEntriesToModifiy = buildRequests.filter((entry) =>
+      activeGroup.includedTypeIDs.includes(entry.itemID)
+    );
+    const itemsToBuild = buildRequests.filter(
+      (entry) => !groupEntriesToModifiy.some((i) => i.itemID === entry.itemID)
+    );
+
+    const newJobData = await buildJob(itemsToBuild);
+
+    for (const job of newJobData) {
+      newPriceIDs = new Set(newPriceIDs, generatePriceRequestFromJob(job));
+      jobsToSave.add(job.jobID);
+
+      job.build.materials.forEach((material) => {
+        let materialMatch = newJobArray.find(
+          (i) =>
+            i.itemID === material.typeID && i.groupID === activeGroup.groupID
+        );
+        if (materialMatch) {
+          materialMatch.parentJob.push(job.jobID);
+          material.childJob.push(materialMatch.jobID);
+          jobsToSave.add(materialMatch.jobID);
+        }
+      });
+    }
+
+    const itemPriceRequest = [getItemPrices([...newPriceIDs], parentUser)];
+
+    for (const entry of groupEntriesToModifiy) {
+      const job = newJobArray.find((i) => i.itemID === entry.itemID);
+      if (!job) continue;
+      const newQuantity = job.build.products.totalQuantity + entry.itemQty;
+
+      recalculateItemQty(job, newQuantity);
+
+      job.build.materials = CalculateResources({
+        jobType: job.jobType,
+        rawMaterials: job.rawData.materials,
+        outputMaterials: job.build.materials,
+        runCount: job.runCount,
+        jobCount: job.jobCount,
+        bpME: job.bpME,
+        structureType: job.structureType,
+        rigType: job.rigType,
+        systemType: job.systemType,
+      });
+      job.build.products.totalQuantity =
+        job.rawData.products[0].quantity * job.runCount * job.jobCount;
+
+      job.build.products.quantityPerJob =
+        job.rawData.products[0].quantity * job.jobCount;
+
+      job.build.time = CalculateTime({
+        jobType: job.jobType,
+        CharacterHash: job.build.buildChar,
+        structureTypeDisplay: job.structureTypeDisplay,
+        runCount: job.runCount,
+        bpTE: job.bpTE,
+        rawTime: job.rawData.time,
+        skills: job.skills,
+      });
+      jobsToSave.add(job.jobID);
+    }
+
+    const {
+      newIncludedTypeIDs,
+      newIncludedJobIDs,
+      newOuputJobCount,
+      newMaterialIDs,
+    } = newJobData.reduce(
+      (prev, entry) => {
+        prev.newIncludedJobIDs.add(entry.jobID);
+        prev.newIncludedTypeIDs.add(entry.itemID);
+        prev.newOuputJobCount++;
+
+        entry.build.materials.forEach((mat) => {
+          prev.newMaterialIDs.add(mat.typeID);
+        });
+
+        return prev;
+      },
+      {
+        newIncludedTypeIDs: new Set(),
+        newIncludedJobIDs: new Set(),
+        newOuputJobCount: 1,
+        newMaterialIDs: new Set(),
+      }
+    );
+
+    const itemPriceData = await Promise.all(itemPriceRequest);
+    newJobArray = newJobArray.concat(newJobData);
+    updateActiveGroup((prev) => ({
+      ...prev,
+      includedJobIDs: [
+        ...new Set([...prev.includedJobIDs, ...newIncludedJobIDs]),
+      ],
+      includedTypeIDs: [
+        ...new Set([...prev.includedTypeIDs, ...newIncludedTypeIDs]),
+      ],
+      outputJobCount: prev.outputJobCount + newOuputJobCount,
+      materialIDs: [...new Set([...prev.materialIDs, ...newMaterialIDs])],
+    }));
+    updateJobArray(newJobArray);
+    updateEvePrices((prev) => {
+      let newEvePrices = itemPriceData[0].filter(
+        (n) => !prev.some((p) => p.typeID === n.typeID)
+      );
+      return prev.concat(newEvePrices);
+    });
+    if (isLoggedIn) {
+      jobsToSave.forEach((id) => {
+        let matchedJob = newJobArray.find((i) => i.jobID === id);
+        if (!matchedJob) return;
+        addNewJob(matchedJob);
+      });
+    }
+  };
+
   return {
+    finalBuildRequests,
     importFromClipboard,
   };
 }

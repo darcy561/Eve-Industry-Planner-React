32f041374f862c2eb436332cf16c95d58cb6b5f5
James Darcy <darcy561@gmail.com>
2023-05-06 10:44:39 +0100

    Commit

--- functions/Scheduled Functions/refreshItemPrices.js
+++ functions/Scheduled Functions/refreshItemPrices.js
@@ -3,72 +3,77 @@ const admin = require("firebase-admin");
 const ESIMarketQuery = require("../sharedFunctions/priceData").ESIMarketQuery;
 const axios = require("axios");
 
+const EVE_SERVER_STATUS_API =
+  "https://esi.evetech.net/latest/status/?datasource=tranquility";
+const MARKET_PRICES_REF = "market-prices";
+const TIME_LIMIT = 4 * 60 * 60 * 1000; // 4 hours
+const MAX_ITEMS = 150;
+
 exports.scheduledFunction = functions
   .region("europe-west1")
   .runWith({ timeoutSeconds: 540 })
   .pubsub.schedule("every 30 minutes")
   .timeZone("Etc/GMT")
   .onRun(async (context) => {
-    const pricingSnapshot = await admin
-      .database()
-      .ref("market-prices")
-      .orderByChild("lastUpdated")
-      .endAt(Date.now() - 14400000)
-      .limitToLast(150)
-      .once("value");
+    try {
+      const pricingSnapshot = await admin
+        .database()
+        .ref(MARKET_PRICES_REF)
+        .orderByChild("lastUpdated")
+        .endAt(Date.now() - TIME_LIMIT)
+        .limitToLast(MAX_ITEMS)
+        .once("value");
 
-    let pricingData = pricingSnapshot.val();
+      let pricingData = pricingSnapshot.val();
 
-    if (pricingData === null) {
-      return null;
-    }
-
-    const server = await axios.get(
-      "https://esi.evetech.net/latest/status/?datasource=tranquility"
-    );
-
-    if (server.status !== 200) {
-      functions.logger.error(
-        "Eve Servers Offline - Unable To Refresh Item Prices"
-      );
-      return null;
-    }
-
-    let successRefreshCount = 0;
-    let failedRefreshCount = 0;
-    const refreshedIDs = [];
-    const failedIDs = [];
-
-    for (let { typeID } in pricingData) {
-      let response = await ESIMarketQuery(typeID, false, pricingData[typeID]);
-
-      if (response === "fail") {
-        failedRefreshCount++;
-        failedIDs.push(typeID);
-      } else {
-        successRefreshCount++;
-        refreshedIDs.push(typeID);
-        pricingData[typeID] = response;
+      if (!pricingData) {
+        return null;
       }
-    }
 
-    if (refreshedIDs.length > 0) {
-      await admin.database().ref("market-prices").update(pricingData);
-    }
+      const server = await axios.get(EVE_SERVER_STATUS_API);
 
-    functions.logger.info(
-      `Number of TypeID's Refreshed ${successRefreshCount}. TypeID's Refreshed ${JSON.stringify(
-        refreshedIDs
-      )}`
-    );
+      if (server.status !== 200) {
+        functions.logger.warn(
+          "Eve Servers Offline - Unable To Refresh Item Prices"
+        );
+        return null;
+      }
 
-    if (failedRefreshCount > 0) {
-      functions.logger.info(
-        `Number of TypeID's Failed ${failedRefreshCount}. TypeID's Failed ${JSON.stringify(
-          failedIDs
+      let successRefreshCount = 0;
+      let failedRefreshCount = 0;
+      const refreshedIDs = [];
+      const failedIDs = [];
+
+      for (const [typeID] of Object.entries(pricingData)) {
+        const response = await ESIMarketQuery(typeID, false);
+        if (response === "fail") {
+          failedRefreshCount++;
+          failedIDs.push(typeID);
+        } else {
+          successRefreshCount++;
+          refreshedIDs.push(typeID);
+          pricingData[typeID] = response;
+        }
+      }
+      if (refreshedIDs.length > 0) {
+        await admin.database().ref(MARKET_PRICES_REF).update(pricingData);
+      }
+
+      functions.logger.log(
+        `Number of TypeID's Refreshed ${successRefreshCount}. TypeID's Refreshed ${JSON.stringify(
+          refreshedIDs
         )}`
       );
+      if (failedRefreshCount > 0) {
+        functions.logger.log(
+          `Number of TypeID's Failed ${failedRefreshCount}. TypeID's Failed ${JSON.stringify(
+            failedIDs
+          )}`
+        );
+      }
+      return null;
+    } catch (err) {
+      functions.logger.error(`An error occured: ${err}`);
+      return null;
     }
-
-    return null;
   });
--- functions/sharedFunctions/priceData.js
+++ functions/sharedFunctions/priceData.js
@@ -1,65 +1,91 @@
+const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const axios = require("axios");
 
-async function ESIMarketQuery(typeID, newItem, outdatedDoc) {
+async function ESIMarketQuery(typeID, newItem) {
+  const maxPages = 50;
+  const maxEntries = 1000;
   const locations = [
     { name: "jita", regionID: 10000002, stationID: 60003760 },
     { name: "amarr", regionID: 10000043, stationID: 60008494 },
     { name: "dodixie", regionID: 10000032, stationID: 60011866 },
   ];
-  let dbObject = { typeID: Number(typeID), lastUpdated: Date.now() };
-  for (let location of locations) {
-    let pageCount = 1;
-    let buyOrders = [];
-    let sellOrders = [];
-    while (pageCount <= 50) {
+
+  const dbObject = { typeID: Number(typeID), lastUpdated: Date.now() };
+  const promises = locations.map((location) =>
+    fetchMarketOrders(typeID, location)
+  );
+  const responses = await Promise.all(promises);
+
+  for (let i = 0; i < responses.length; i++) {
+    const location = locations[i];
+    const orders = responses[i];
+
+    if (!orders) {
+      return "fail";
+    }
+    const { buyOrders, sellOrders } = filterOrders(orders, location.stationID);
+    const { highestBuyPrice, lowestSellPrice } =
+      getHighestBuyAndLowestSellPrices(buyOrders, sellOrders);
+    dbObject[location.name] = { buy: highestBuyPrice, sell: lowestSellPrice };
+  }
+  await saveMarketPricesToDatabase(typeID, dbObject);
+
+  return dbObject;
+
+  async function fetchMarketOrders(typeID, location) {
+    let orders = [];
+    for (let pageCount = 1; pageCount <= maxPages; pageCount++) {
       try {
         const response = await axios.get(
           `https://esi.evetech.net/latest/markets/${location.regionID}/orders/?datasource=tranquility&order_type=all&page=${pageCount}&type_id=${typeID}`
         );
-        if (response.status === 200) {
-          response.data.forEach((order) => {
-            if (order.location_id === location.stationID) {
-              if (order.is_buy_order) {
-                buyOrders.push(order);
-              } else {
-                sellOrders.push(order);
-              }
-            }
-          });
-          if (response.data.length < 1000) {
-            pageCount = 51;
-          } else {
-            pageCount++;
-          }
-        } else {
-          pageCount = 51;
-          return "fail";
+        if (response.status !== 200) break;
+        orders.push(...response.data);
+        if (response.data.length < maxEntries) {
+          break;
         }
       } catch (err) {
-        pageCount = 51;
-        return "fail";
+        functions.logger.error(err);
+        break;
       }
     }
-    if (buyOrders.length > 0) {
-      buyOrders.sort((a, b) => {
-        return b.price - a.price;
-      });
-    }
-    if (sellOrders.length > 0) {
-      sellOrders.sort((a, b) => {
-        return a.price - b.price;
-      });
-    }
-    dbObject[location.name] = {
-      buy: buyOrders.length ? buyOrders[0].price : 0,
-      sell: sellOrders.length ? sellOrders[0].price : 0,
-    };
+    return orders;
   }
-  if (newItem) {
-    await admin.database().ref(`market-prices/${typeID}`).set(dbObject);
+
+  function filterOrders(orders, stationID) {
+    const buyOrders = [];
+    const sellOrders = [];
+
+    for (let order of orders) {
+      if (order.location_id === stationID) {
+        if (order.is_buy_order) {
+          buyOrders.push(order);
+        } else {
+          sellOrders.push(order);
+        }
+      }
+    }
+    return { buyOrders, sellOrders };
   }
-  return dbObject;
+
+  function getHighestBuyAndLowestSellPrices(buyOrders, sellOrders) {
+    const highestBuyPrice =
+      buyOrders.length > 0 ? Math.max(...buyOrders.map((o) => o.price)) : 0;
+    const lowestSellPrice =
+      sellOrders.length > 0 ? Math.min(...sellOrders.map((o) => o.price)) : 0;
+    return { highestBuyPrice, lowestSellPrice };
+  }
+
+  async function saveMarketPricesToDatabase(typeID, marketPrices) {
+    try {
+      if (newItem) {
+        await admin.database().ref(`market-prices/${typeID}`).set(marketPrices);
+      }
+    } catch (err) {
+      functions.logger.error(err);
+    }
+  }
 }
 
 module.exports = {

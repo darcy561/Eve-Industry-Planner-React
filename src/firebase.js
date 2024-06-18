dad2e8a6873fa7354e07f2212109418673ac77c3
Oswold Saraki <86856265+darcy561@users.noreply.github.com>
2024-06-18 22:05:53 +0100

    v0.7.1

--- src/firebase.js
+++ src/firebase.js
@@ -40,7 +40,9 @@ export const performance = getPerformance(app);
 export const analytics = getAnalytics(app);
 
 export const remoteConfig = getRemoteConfig(app);
-remoteConfig.settings.minimumFetchIntervalMillis = 300000; //5mins
+if (import.meta.env.DEV) {
+  remoteConfig.settings.minimumFetchIntervalMillis = 300000; //5mins
+}
 remoteConfig.defaultConfig = REMOTE_CONFIG_DEFAULT_VALUES;
 fetchAndActivate(remoteConfig);
 export default app;
